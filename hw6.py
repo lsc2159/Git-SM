22aebaed8d41a29720194919bd4d0305a49a95ff
lsc2159 <127822509+lsc2159@users.noreply.github.com>
2023-04-20 20:59:05 +0900

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,10 @@
+for i in range(1, 10):
+    for j in range (2, 6):
+        print(f'{j}X{i}={j * i:2d}', end=' ')
+    print()
+print()   
+
+for i in range(1, 10):
+    for j in range(6, 10):
+        print(f'{j}X{i}={j * i:2d}', end=' ')
+    print()
